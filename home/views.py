0395950290380285895ad5630c80ff7e78cdbb76
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-19 01:36:03 +0200

    Function for getting users

--- home/views.py
+++ home/views.py
@@ -1,4 +1,41 @@
-from django.shortcuts import render
+from account.models import *
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
+@login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+@login_required
+def getUsers(request):
+    if request.user.role == 'Manager':
+        messages.error(request, "You are not authorized to access this page.")
+        return redirect('base:dashboard')
+
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        if user_id:
+            try:
+                user = User.objects.get(id=user_id)
+                if not user.is_staff:
+                    user.delete()
+                    messages.success(request, "User deleted successfully.")
+                else:
+                    messages.error(request, "You cannot delete a user with the 'is_staff' role.")
+            except User.DoesNotExist:
+                messages.error(request, "User not found.")
+
+    if request.user.role == 'Manager':
+        getUsers = User.objects.filter(role='Manager')
+    elif request.user.role == 'Admin' or request.user.is_superuser:
+        getUsers = User.objects.all()
+    else:
+        getUsers = User.objects.none()
+
+    context = {
+        'getUsers': getUsers,
+        'logged_in_user': request.user
+    }
+
+    return render(request, 'users/index.html', context)
