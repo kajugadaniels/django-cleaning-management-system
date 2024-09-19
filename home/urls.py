50413ee83180ab8986b236c127f606c52f57d852
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-19 07:04:23 +0200

    Routes

--- home/urls.py
+++ home/urls.py
@@ -13,4 +13,13 @@ urlpatterns = [
     path('user/edit/<int:id>/', editUser, name='editUser'),
     path('user/delete/<int:id>/', deleteUser, name='deleteUser'),
 
+    path('cleanup-requests/', getCleanupRequests, name='getCleanupRequests'),
+    path('cleanup-request/add/', addCleanupRequest, name='addCleanupRequest'),
+    path('cleanup-request/edit/<int:id>/', editCleanupRequest, name='editCleanupRequest'),
+    path('cleanup-request/delete/<int:id>/', deleteCleanupRequest, name='deleteCleanupRequest'),
+
+    path('tasks/', getTasks, name='getTasks'),
+    path('task/add/', addTask, name='addTask'),
+    path('task/edit/<int:id>/', editTask, name='editTask'),
+    path('task/delete/<int:id>/', deleteTask, name='deleteTask'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
