be77af03889009ab5c1af5dde097f6c8fc589cfa
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-19 10:37:44 +0200

    Routes for approving requests

--- home/urls.py
+++ home/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
     path('cleanup-requests/', getCleanupRequests, name='getCleanupRequests'),
     path('cleanup-request/add/', addCleanupRequest, name='addCleanupRequest'),
     path('cleanup_requests/<int:cleanup_request_id>/', viewCleanupRequest, name='viewCleanupRequest'),
+
+    path('admin/cleanup-requests/', adminViewCleanupRequests, name='adminViewCleanupRequests'),
+    path('admin/cleanup-requests/<int:request_id>/', adminApproveCleanupRequest, name='adminApproveCleanupRequest'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
