efcd610cde94c15fcb35053714d0290e65efc00e
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-15 18:01:38 +0200

    Add dashboard route and function

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ from home.views import *
 app_name = 'base'
 
 urlpatterns = [
-    
+    path('dashboard', dashboard, name="dashboard"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from urllib.request import Request
+from home.forms import *
+from home.models import *
+from account.models import *
+from django.contrib import messages
+from django.db.models import Count, Q, F  # Import necessary models
+from .models import User, CleanupRequest 
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
