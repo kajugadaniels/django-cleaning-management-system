da631a300db6335f422cbc2d8e1a6711d54a168a
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-15 17:52:14 +0200

    Add route for login

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,8 @@ from django.conf.urls.static import static
 app_name = 'auth'
 
 urlpatterns = [
-    
+    path('', user_login, name="login"),
+    # path('logout/', user_logout, name='logout'),
+
+    # path('profile/', userProfile, name='userProfile'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
