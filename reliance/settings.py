27c7582d5550f625b145b27dc6e88650f89c1e4f
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-22 18:46:23 +0200

    New changes

--- reliance/settings.py
+++ reliance/settings.py
@@ -72,10 +72,13 @@ WSGI_APPLICATION = 'reliance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("MYSQL_DB"),
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST"),
+        'PORT': os.getenv("MYSQL_PORT", "3306"),
     }
-}
 
 
 # Password validation
